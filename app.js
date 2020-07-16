9411de33a255c4a2e2d2e45c2e5448e514726d47
Darren Behan <darrenbehan@hotmail.com>
2020-07-16 20:59:47 +1000

    redesigned inquirer prompts as they were ending after initial input of employee data - I couldn't capture extra employee data. Added a loop based on user input of number of employees required to capture the necessary amount of employee data.

--- app.js
+++ app.js
@@ -9,100 +9,186 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { listenerCount } = require("process");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// function to prompt the user with a series of questions to gather data for the file being created
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your id?"
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email?"
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "What is your current role?",
-      choices: [
-        "Engineer",
-        "Manager",
-        "Intern"
-      ]
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is your Github username?",
-      when: function(answers) {
-        return answers.role === "Engineer";
-      }
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What school did you attend?",
-      when: function(answers) {
-        return answers.role === "Intern";
-      }
-    },
-    {
-      type: "input",
-      name: "officePhoneNumber",
-      message: "What is your office phone number?",
-      when: function(answers) {
-        return answers.role === "Manager";
-      }
-    }
-  ]);
-}
-
-async function init() {
-  try {
-    // init function pauses whilst gathering user data through the promptUser function and stores the data in "answers"
-    const answers = await promptUser();
-
-    // `render` function will generate and return a block of HTML including templated divs for each employee
-    render(answers);
-
-    // notifies the user if successful
-    console.log("Successful");
-  } catch (err) {
-    // notifies the user if there was an error
-    console.log(err);
-  }
-}
+let numberOfEmployees = [];
+const employeeData = [];
 
 init();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+async function promptUser(number) {
+  console.log("The first team member to add will be your Manager.");
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name_manager",
+        message: "What is your Manager's name?",
+      },
+      {
+        type: "input",
+        name: "id_manager",
+        message: "What is your Manager's id?",
+      },
+      {
+        type: "input",
+        name: "email_manager",
+        message: "What is your Manager's email?",
+      },
+      {
+        type: "input",
+        name: "officePhoneNumber",
+        message: "What is your Manager's office phone number?",
+      },
+    ])
+    .then((response) => {
+      const {
+        name_manager,
+        id_manager,
+        email_manager,
+        officePhoneNumber,
+      } = response;
+      const manager = new Manager(
+        name_manager,
+        id_manager,
+        email_manager,
+        officePhoneNumber
+      );
+      employeeData.push(manager);
+    });
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+  for (let i = 0; i < number - 1; i++) {
+    await inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "role",
+          message: "Which additional team member would you like to add?",
+          choices: ["Engineer", "Intern"],
+        },
+        {
+          type: "input",
+          name: "name_engineer",
+          message: "What is your Engineer's name?",
+          when: (answer) => {
+            return answer.role === "Engineer";
+          },
+        },
+        {
+          type: "input",
+          name: "id_engineer",
+          message: "What is your Engineer's id?",
+          when: (answer) => {
+            return answer.role === "Engineer";
+          },
+        },
+        {
+          type: "input",
+          name: "email_engineer",
+          message: "What is your Engineer's email?",
+          when: (answer) => {
+            return answer.role === "Engineer";
+          },
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is your Github username?",
+          when: (answer) => {
+            return answer.role === "Engineer";
+          },
+        },
+        {
+          type: "input",
+          name: "name_intern",
+          message: "What is your Intern's name?",
+          when: (answer) => {
+            return answer.role === "Intern";
+          },
+        },
+        {
+          type: "input",
+          name: "id_intern",
+          message: "What is your Intern's id?",
+          when: (answer) => {
+            return answer.role === "Intern";
+          },
+        },
+        {
+          type: "input",
+          name: "email_intern",
+          message: "What is your Intern's email?",
+          when: (answer) => {
+            return answer.role === "Intern";
+          },
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What is your Intern's school?",
+          when: (answer) => {
+            return answer.role === "Intern";
+          },
+        },
+      ])
+      .then((response) => {
+        const {
+          name_engineer,
+          id_engineer,
+          email_engineer,
+          github,
+          name_intern,
+          id_intern,
+          email_intern,
+          school,
+        } = response;
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        if (name_engineer !== undefined) {
+          const engineer = new Engineer(
+            name_engineer,
+            id_engineer,
+            email_engineer,
+            github
+          );
+          employeeData.push(engineer);
+        }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+        if (name_intern !== undefined) {
+          const intern = new Intern(
+            name_intern, 
+            id_intern, 
+            email_intern, 
+            school
+          );
+          employeeData.push(intern);
+        }
+      });
+  }
+}
+
+async function numberOfTeamMembers() {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "team_total",
+        message: "How many members are in your team?",
+      },
+    ])
+    .then((response) => {
+      const number = response.team_total;
+      numberOfEmployees.push(number);
+    });
+}
+
+// function to prompt the user with a series of questions to gather data for the file being created
+async function init() {
+  console.log("Let's build your team 🛠");
+
+  await numberOfTeamMembers();
+  const number = numberOfEmployees[0];
+
+  await promptUser(number);
+  console.log(employeeData);
+  // render(employeeData);
+}
